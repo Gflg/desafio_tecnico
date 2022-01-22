88c60b2b6038aebebe7dd5c19b654553d83cfd45
Gflg <gflgon@gmail.com>
2022-01-22 18:40:18 -0300

    Alterando flag de debug

--- desafio_deloitte/settings.py
+++ desafio_deloitte/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2i=svv$vuzr&0g6*7_hlbc3qcq!*8x-@_vgtkvo-%(26*lrk38'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
