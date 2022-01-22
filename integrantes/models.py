51d418428bfc7cafe92f1e7f1f853e2bb4affd2f
Gflg <gflgon@gmail.com>
2022-01-22 17:59:53 -0300

    Alterando modelos

--- integrantes/models.py
+++ integrantes/models.py
@@ -2,14 +2,12 @@ from django.db import models
 from uuid import uuid4
 from servicos.models import Servicos
 
-DEFAULT_SERVICE_AREA_ID = 1
-
 
 class Integrantes(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=100)
     birth_date = models.DateField(null=False)
     address = models.CharField(max_length=500)
-    service_area = models.ForeignKey(Servicos, on_delete=models.CASCADE, null=False, default=DEFAULT_SERVICE_AREA_ID)
+    service_area = models.ForeignKey(Servicos, on_delete=models.CASCADE)
     is_allocated = models.BooleanField(null=False, default=False)
     joined_at = models.DateTimeField(auto_now_add=True)
--- posts/models.py
+++ posts/models.py
@@ -2,13 +2,11 @@ from django.db import models
 from uuid import uuid4
 from integrantes.models import Integrantes
 
-DEFAULT_AUTHOR_ID = 1
-
 
 class Posts(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=1000)
-    author = models.ForeignKey(Integrantes, on_delete=models.CASCADE, null=False, default=DEFAULT_AUTHOR_ID)
+    author = models.ForeignKey(Integrantes, on_delete=models.CASCADE)
     theme = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
